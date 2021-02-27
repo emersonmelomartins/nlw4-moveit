77f1a65988a1dd466e573c10307f8e1f8f35b6fd
Emerson Melo Martins <emerson25xd@gmail.com>
2021-02-27 10:30:46 -0300

    NLW4 React - ContextAPI e Components

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,48 +1,86 @@
-import { useState, useEffect } from 'react';
-import styles from '../styles/components/Countdown.module.css';
+import { useState, useEffect, useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Countdown.module.css";
+
+let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+  const [timeInSeconds, setTimeInSeconds] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
-    const [timeInSeconds, setTimeInSeconds] = useState(25 * 60);
-    const [active, setActive] = useState(false);
+  const { startNewChallenge } = useContext(ChallengesContext);
 
-    const minutes = Math.floor(timeInSeconds / 60);
-    const seconds = timeInSeconds % 60;
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = timeInSeconds % 60;
 
-    // padStart -> verifica se não tem 2 caracteres e preenche com 0
-    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
-    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
+  const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
 
-    useEffect(() => {
-        if(active && timeInSeconds > 0) {
-            setTimeout(() => {
-                setTimeInSeconds(timeInSeconds - 1);
-            }, 1000);
-        }
-    }, [active, timeInSeconds]);
-
-    function startCountdown() {
-        setActive(true);
+  useEffect(() => {
+    if (isActive && timeInSeconds > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTimeInSeconds(timeInSeconds - 1);
+      }, 1000);
+    } else if (isActive && timeInSeconds === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
     }
+  }, [isActive, timeInSeconds]);
 
-    return (
+  function startCountdown() {
+    setIsActive(true);
+  }
 
+  function resetCountdown() {
+    setIsActive(false);
+    clearTimeout(countdownTimeout);
+    setTimeInSeconds(25 * 60);
+  }
+
+  return (
+    <div>
+      <div className={styles.countdownContainer}>
         <div>
-            <div className={styles.countdownContainer}>
-                <div>
-                    <span>{minuteLeft}</span>
-                    <span>{minuteRight}</span>
-                </div>
-                <span>:</span>
-                <div>
-                    <span>{secondLeft}</span>
-                    <span>{secondRight}</span>
-                </div>
-            </div>
-
-            <button type="button" className={styles.countdownButton} onClick={startCountdown}>
-                Iniciar um ciclo
-            </button>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
-    );
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </div>
+
+      {
+        // Forma de operador ternário sem "else"
+        hasFinished ? (
+          <button disabled className={`${styles.countdownButton}`}>
+            Ciclo encerrado
+          </button>
+        ) : (
+          <>
+            {isActive ? (
+              <button
+                type="button"
+                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                onClick={resetCountdown}
+              >
+                Abandonar ciclo
+              </button>
+            ) : (
+              <button
+                type="button"
+                className={styles.countdownButton}
+                onClick={startCountdown}
+              >
+                Iniciar um ciclo
+              </button>
+            )}
+          </>
+        )
+      }
+    </div>
+  );
 }
